1774ff43d2308966969ae5ca839a1975cfaeb012
Kurtis LoVerde <kurt@loverde.org>
2016-03-25 18:45:16 -0400

    Good time to check in current progress

---
+++ README.md
@@ -0,0 +1,75 @@
+# This project is an unfinished work in progress.  Check back soon.
+
+
+PickAndChoose
+=============
+
+See LICENSE for this software's licensing terms.
+
+PickAndChoose is a jQuery plugin which allows you make selections from a list of displayed items.  There are two boxes:  one box contains a list of available options and the other displays the items you've selected.  You select and deselect options by using buttons that sit between the two boxes.  When you select an option, it moves from the 'available' box to the the 'selected' box.  When you deselect an option, the opposite happens.  You can also select and deselect all options at once.
+
+Setup is incredibly simple:  invoke the plugin on an empty <div> and pass in the values for the selection boxes in JSON format.  The plugin takes care of the rest.
+
+
+## Features
+
+* Supports unlimited simultaneous instances
+* Definable CSS classes make the widget highly styleable
+* Supports a callback on change
+
+
+## Using
+
+Invoke the plugin on an empty <div> and pass properties as you prefer.  The only required properties are `unselectedItems` and `selectedItems`.  If you have more than one PickAndChoose widget in your page, you must then specify `unselectedId` and `selectedItems`.
+
+| Property | Description | Default Value |
+| ----------------- | --------------------------------------------------------------------------------------------------------------------------- |---------------|
+| `containerClass` | The class name of the <div> which contains everything | pacContainer |
+| `unselectedContainerClass` | The class name of the <div> which contains the <select> of unselected items | pacUnselectedContainer |
+| `buttonContainerClass` | The class name of the <div> containing the buttons | pacButtonContainer |
+| `buttonSelectClass` | The class of the select <button> | pacButtonSelect |
+| `buttonSelectAllClass` | The class of the select-all <button> | pacButtonSelectAll |
+| `buttonDeselectClass` | The class of the deselect <button> | pacButtonDeselect |
+| `buttonDeselectAllClass` | The class of the deselect-all <button> | pacButtonDeselectAll |
+| `selectedContainerClass` | The class name of the <div> containing the user selections <select> | pacSelectedContainer |
+| `buttonSelectText` | The text of the select button | > |
+| `buttonSelectAllText` | The text of the select-all button | >> |
+| `buttonDeselectText` | The text of the deselect button |  < |
+| `buttonDeselectAllText` | The text of the deselect-all button | << |
+| `unselectedId` | The ID of the <select> containing the unselected options | unselected |
+| `selectedId` | The ID of the <select> containing the selected options | selected |
+| `unselectedItems` | JSON of unselected items to populate the unselected items' <select> with | null |
+| `selectedItems` | JSON of items to populate the selected items' <select> with | null |
+| `onChangeCallback | A callback function to execute when the user uses the buttons | null |
+| `showErrors` | Determines whether to display initialization errors in the page.  If set to true, your users will see them, so you might choose to treat this as a debug option.  Regardless of the setting, the plugin will throw initialization errors as exceptions. | false |
+
+
+#### Example
+
+```html
+
+<div id="myPicker"></div>
+
+```
+
+```javascript
+
+$( "#myPicker" ).pickAndChoose( {
+   unselectedItems : {
+                       "Choice 1" : "choice1",
+                       "Choice 3" : "choice3"
+                     },
+
+   selectedItems   : {
+                        "Choice 2" : "choice2"
+                     }
+} );
+
+```
+
+See the included HTML file for a more in-depth demo.
+
+
+## Thanks
+
+Do you like this library?  Want to toss a few bucks my way to say thanks?  I accept donations at https://paypal.me/KurtisLoVerde/5.  Thank you for your support!
---
+++ jquery.pickandchoose.js
@@ -0,0 +1,201 @@
+/*
+ * PickAndChoose v1.0
+ * https://www.github.com/kloverde/jquery-PickAndChoose
+ *
+ * This software is licensed under the 3-clause BSD license.
+ *
+ * Copyright (c) 2016 Kurtis LoVerde
+ * All rights reserved
+ */
+
+(function( $ ) {
+   $.fn.pickAndChoose = function( options ) {
+
+      var settings = $.extend( {
+         // The class name of the <div> which contains everything
+         containerClass : "pacContainer",
+
+         // The class name of the <div> which contains the <select> of unselected items
+         unselectedContainerClass : "pacUnselectedContainer",
+
+         // The class name of the <div> containing the buttons
+         buttonContainerClass : "pacButtonContainer",
+
+         // The class of the select <button>
+         buttonSelectClass : "pacButtonSelect",
+
+         // The class of the select-all <button>
+         buttonSelectAllClass : "pacButtonSelectAll",
+
+         // The class of the deselect <button>
+         buttonDeselectClass : "pacButtonDeselect",
+
+         // The class of the deselect-all <button>
+         buttonDeselectAllClass : "pacButtonDeselectAll",
+
+         // The class name of the <div> containing the user selections <select>
+         selectedContainerClass : "pacSelectedContainer",
+
+         // The text of the select button
+         buttonSelectText : ">",
+
+         // The text of the select-all button
+         buttonSelectAllText : ">>",
+
+         // The text of the deselect button
+         buttonDeselectText : "<",
+
+         // The text of the deselect-all button
+         buttonDeselectAllText : "<<",
+
+         // The ID of the <select> containing the unselected options
+         unselectedId : "unselected",
+
+         // The ID of the <select> containing the selected options
+         selectedId : "selected",
+
+         // JSON of items to populate the unselected items' <select> with
+         unselectedItems : null,
+
+         // JSON of items to populate the selected items' <select> with
+         selectedItems : null,
+
+         // A callback function to execute when the user uses the buttons
+         onChangeCallback : null,
+
+         // Determines whether to display initialization errors in the page.
+         // If set to true, your users will see them, so you might choose to
+         // treat this as a debug option.  Regardless of the setting, the
+         // plugin will throw initialization errors as exceptions.
+         showErrors : false
+      }, options );
+
+      var GROUP_STATE_NONE = "none",
+          GROUP_STATE_SOME = "some",
+          GROUP_STATE_ALL  = "all";
+
+      buildWidget( this );
+
+      function buildWidget( container ) {
+         // Scan for keys that appear in both the unselected and selected data sets
+
+         $.each( settings.unselectedItems, function(prevKey, prevValue) {
+            $.each( settings.selectedItems, function(key, value) {
+               if( prevKey === key ) {
+                  var errMsg = "MultipleSelect initialization error for "
+                             + $( container ).attr( "id" )
+                             + ":  key '" + prevKey
+                             + "' appears in both unselected and selected items"
+
+                  if( settings.showErrors ) {
+                     container.append( errMsg );
+                  }
+
+                  throw errMsg;
+               }
+            } );
+         } );
+
+         // create containers
+
+         var unselectedContainer = $( "<div/>", {
+            class : settings.unselectedContainerClass
+         } );
+
+         var buttonContainer = $( "<div/>", {
+            class : settings.buttonContainerClass
+         } );
+         
+         var selectedContainer = $( "<div/>", {
+            class : settings.selectedContainerClass,
+         } );
+
+         // create unselected container content
+
+         var unselectedSelect = $( "<select/>", {
+            id : settings.unselectedId,
+            multiple : ""
+         } );
+
+         if( settings.unselectedItems != null ) {
+            $.each( settings.unselectedItems, function(key, value) {   
+               unselectedSelect.append( $("<option></option>")
+                                          .text(key)
+                                          .attr("value", value) );
+            } );
+         }
+
+         unselectedSelect.appendTo( unselectedContainer );
+
+         // create button container content
+         
+         var btnSelect = $( "<button/>", {
+            class : settings.buttonSelectClass,
+            text : settings.buttonSelectText
+         } );
+
+         var btnSelectAll = $( "<button/>", {
+            class : settings.buttonSelectAllClass,
+            text : settings.buttonSelectAllText
+         } );
+
+         var btnDeselect = $( "<button/>", {
+            class : settings.buttonDeselectClass,
+            text : settings.buttonDeselectText
+         } );
+
+         var btnDeselectAll = $( "<button/>", {
+            class : settings.buttonDeselectAllClass,
+            text : settings.buttonDeselectAllText
+         } );
+
+         if( typeof settings.onChangeCallback === "function" ) {
+            btnSelect.on( "click", function() {
+               settings.onSelectCallback();
+            } );
+
+            btnSelectAll.on( "click", function() {
+               settings.onSelectCallback();
+            } );
+
+            btnDeselect.on( "click", function() {
+               settings.onDeselectCallback();
+            } );
+
+            btnDeselectAll.on( "click", function() {
+               settings.onDeselectCallback();
+            } );
+         };
+
+         btnSelect.appendTo( buttonContainer );
+         btnSelectAll.appendTo( buttonContainer );
+         btnDeselect.appendTo( buttonContainer );
+         btnDeselectAll.appendTo( buttonContainer );
+
+         // create selected container content
+
+         var selectedSelect = $( "<select/>", {
+            multiple : "",
+            id : settings.selectedId
+         } );
+
+         if( settings.selectedItems != null ) {
+            $.each( settings.selectedItems, function(key, value) {   
+               selectedSelect.append( $("<option></option>")
+                                          .text(key)
+                                          .attr("value", value) );
+            } );
+         }
+
+         selectedSelect.appendTo( selectedContainer );
+
+         // add elements to the page
+         
+         container.addClass( settings.containerClass );
+
+         container.append( unselectedContainer );
+         container.append( buttonContainer );
+         container.append( selectedContainer );
+      }
+   };
+}( jQuery ));
